14526e22542e9ea1f7cde946c474a2e951c73f88
F. Giovanni Sanguineti <franco.sanguineti@ug.uchile.cl>
2019-07-21 16:46:28 -0400

    updated aux2/maxsubarray

--- exercises/aux2/maxsubarray.py
+++ exercises/aux2/maxsubarray.py
@@ -5,31 +5,33 @@ def subarreglo_maximo(arreglo, i, j):
     # caso base, si es que el arreglo es de largo 1, el máximo es el elemento solo.
     if i == j:
         return arreglo[0]
+    # calcular recursivamente el máximo de las 2 mitades.
     m = (j + i) // 2
     suma_max_izq = subarreglo_maximo(arreglo, i, m)
     suma_max_der = subarreglo_maximo(arreglo, m + 1, j)
 
+    # calcular el máximo subarreglo que cruza la mitad.
     max_i = 0
     max_d = 0
 
+    # máximo a la izquierda. Se parte al medio y se avanza hacia el principio.
     suma = 0
     x = m
-
     while x >= i:
         suma += arreglo[x]
         if suma > max_i:
             max_i = suma
         x -= 1
 
+    # máximo a la derecha. Se parte al medio y se avanza hacia el final.
     suma = 0
     x = m + 1
-
     while x <= j:
         suma += arreglo[x]
         if suma > max_d:
             max_d = suma
         x += 1
-
+    # se retorna el máximo entre las 2 mitades y el subarreglo que cruza la mitad (max_i + max_d)
     return max(max_d + max_i, max(suma_max_der, suma_max_izq))
 
 
