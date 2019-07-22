b32230b536dece051e43feb7127a66e3ede39248
F. Giovanni Sanguineti <franco.sanguineti@ug.uchile.cl>
2019-07-22 13:48:02 -0400

    updated p2 aux1

--- exercises/aux1/p2.py
+++ exercises/aux1/p2.py
@@ -3,7 +3,7 @@ __email__ = "franco.sanguineti@ug.uchile.cl"
 
 import numpy as np
 
-from aux1.p1 import Pokemon
+from p1 import Pokemon
 
 # Exercise a
 
@@ -18,41 +18,47 @@ array_b = np.arange(5, 14).reshape(3, 3)
 print(array_b, "\n")
 
 # Exercise c
-
-array_c = np.arange(32, 68)  # size: 36
-print("Exercise c array : ", array_c)
-array_c_inverted = np.ones(36, dtype=int)  # creates an array with 36 ones
-for number in range(36):
-    array_c_inverted[35 - number] = array_c[number]
-print("Inverted Exercise c array : ", array_c_inverted)
-print("Size of Inverted Exercise c array: ", array_c.shape)
-reshaped_Exercise_c_array = array_c_inverted.reshape(6, 6)
-print(reshaped_Exercise_c_array)
-print("Size of Inverted Exercise c array: ", reshaped_Exercise_c_array.shape, "\n")
+array_c = np.zeros((5, 5))
+for i in range(len(array_c)):
+    for j in range(len(array_c[i])):
+        array_c[i][j] = 2
+print(array_c)
 
 # Exercise d
 
-array_d = np.empty(5, dtype='U20')
-array_d[0] = "Hola amig@"
-array_d[4] = "Adiós amig@"
-print(array_d, "\n")
+array_d = np.arange(32, 68)  # size: 36
+print("Exercise d array : ", array_d)
+array_d_inverted = np.ones(36, dtype=int)  # creates an array with 36 ones
+for number in range(36):
+    array_d_inverted[35 - number] = array_d[number]
+print("Inverted Exercise d array : ", array_d_inverted)
+print("Size of Inverted Exercise d array: ", array_d.size)
+reshaped_Exercise_d_array = array_d_inverted.reshape(6, 6)
+print(reshaped_Exercise_d_array)
+print("Size of Inverted Exercise d array: ", reshaped_Exercise_d_array.shape, "\n")
 
 # Exercise e
 
-array_e = np.empty(15, dtype=float)
-array_e[0] = 3.3
-for x in range(1, 15):
-    array_e[x] = array_e[x - 1] + 0.3
+array_e = np.empty(5, dtype='U20')
+array_e[0] = "Hola amig@"
+array_e[4] = "Adiós amig@"
 print(array_e, "\n")
 
 # Exercise f
 
+array_f = np.empty(15, dtype=float)
+array_f[0] = 3.3
+for x in range(1, 15):
+    array_f[x] = array_f[x - 1] + 0.3
+print(array_f, "\n")
+
+# Exercise g
+
 charizard = Pokemon("Charizard", 47, 6)
 gyarados = Pokemon("Gyarados", 113, 130)
 evee = Pokemon("Evee", 45, 133)
-array_f = np.array([charizard, gyarados, evee], dtype=Pokemon)
+array_g = np.array([charizard, gyarados, evee], dtype=Pokemon)
 
-for pokemon in array_f:
+for pokemon in array_g:
     print(pokemon.name)
 print("\n")
-
