8ad87fc28367cdac4fd5d9e810144d4d0757bf1b
F. Giovanni Sanguineti <franco.sanguineti@ug.uchile.cl>
2019-08-28 14:27:24 -0400

    added agregar()

--- exercises/aux5/P1.py
+++ exercises/aux5/P1.py
@@ -43,3 +43,17 @@ class ListaCircular:
         buscador.sig = buscador.sig.sig
         self.size -= 1
         return buscador.sig
+
+        def agregar(self, v):
+            if self.size == 0:
+                self.primero = NodoLista(v)
+                self.primero.sig = self.primero #Lista Circular
+                self.size += 1
+                return
+        
+        nodo_nuevo = NodoLista(v, primero)
+        ultimo_antiguo = self.primero
+        for x in range(1, self.size):
+            ultimo_antiguo = ultimo_antiguo.sig
+            ultimo_antiguo.sig = nodo_nuevo
+        self.size += 1
