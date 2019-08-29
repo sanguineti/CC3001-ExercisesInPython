f544e600d749fc7705101aeef3f29454f5d84343
F. Giovanni Sanguineti <franco.sanguineti@ug.uchile.cl>
2019-08-29 16:57:00 -0400

    added numero nodos

--- exercises/aux5/p3.py
+++ exercises/aux5/p3.py
@@ -3,3 +3,12 @@ class NodoArbolBinario:
         self.val = v
         self.izq = izq
         self.der = der
+
+class ArbolBinario:
+    def __init__(self):
+        self.raiz = None
+
+    def numero_nodos(self, arbol):
+        if arbol.raiz == None:
+            return 0
+        return 1 + self.numero_nodos(arbol.der) + self.numero_nodos(arbol.izq)
