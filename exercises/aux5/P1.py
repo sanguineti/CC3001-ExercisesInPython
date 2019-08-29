e7b976755c31fab4a7e1d41bf836db31f5434b05
F. Giovanni Sanguineti <franco.sanguineti@ug.uchile.cl>
2019-08-29 03:02:06 -0400

    added coments to p1

--- exercises/aux5/P1.py
+++ exercises/aux5/P1.py
@@ -1,7 +1,7 @@
 class NodoLista:
-    def __init__(self, val, sig=None):
-        self.val = val
-        self.sig = sig
+    def __init__(self, v, s=None):
+        self.val = v
+        self.sig = s
 
     def __repr__(self):
         return repr(self.val)
@@ -12,9 +12,11 @@ class ListaCircular:
         self.primero = None
         self.size = 0
 
+    # Retorna la cantidad de elementos en la lista
     def largo(self):
         return self.size
 
+    # Retorna el i-ésimo elemento de la lista, contando desde 1
     def obtener(self, i):
         if self.size == 0:
             return -1
@@ -23,6 +25,7 @@ class ListaCircular:
             buscador = buscador.sig
         return buscador.val
 
+    # Retorna el índice del elemento si existiese, de lo contario retorna -1
     def existe(self, num):
         buscador = self.primero
         for x in range(self.size):
@@ -31,31 +34,36 @@ class ListaCircular:
             buscador = buscador.sig
         return -1
 
+    # Elimina el i-ésimo eleento y retorna la referencia al siguiente nodo del eliminado
     def eliminar(self, i):
         if self.size == 0:
             return None
+        # Avanzar hasta quedar en la posición anterior al elemento a eliminar
         buscador = self.primero
-        if (i == 1):
+        if (i == 1):  # Primer elemento es un caso especial
+            # Recorremos hasta el último nodo para que quede en el nodo anterior al primero
             for x in range(1, self.size):
                 buscador = buscador.sig
         else:
+            # Caso general
             for x in range(1, i - 1):
                 buscador = buscador.sig
+        # Aquí estamos en el nodo anterior al que queremos eliminar, ahora lo eliminamos.
         print("Eliminado: ", buscador.sig.val)
-        buscador.sig = buscador.sig.sig
+        buscador.sig = buscador.sig.sig # Cambio la referencia
         self.size -= 1
-        return buscador.sig
+        return buscador.sig # Retornar el siguiente al eliminado
 
     def agregar(self, v):
-        if self.size == 0:
+        if self.size == 0: # No elementos
             self.primero = NodoLista(v)
             self.primero.sig = self.primero  # Lista Circular
             self.size += 1
             return
-
-        nodo_nuevo = NodoLista(v, self.primero)
+        # Si existen elementos
+        nodo_nuevo = NodoLista(v, self.primero) #Como lo inserto al final, apunta al primero
         ultimo_antiguo = self.primero
         for x in range(1, self.size):
             ultimo_antiguo = ultimo_antiguo.sig
-        ultimo_antiguo.sig = nodo_nuevo
+        ultimo_antiguo.sig = nodo_nuevo # Ahora apunta al nuevo
         self.size += 1
