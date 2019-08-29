f95d45a8ee7953ce69684bc0261303585ad5cacd
F. Giovanni Sanguineti <franco.sanguineti@ug.uchile.cl>
2019-08-29 18:00:45 -0400

    added is_arbol_binario

--- exercises/aux5/p3.py
+++ exercises/aux5/p3.py
@@ -1,6 +1,9 @@
 import sys
 import math
 
+MAX_VALUE = sys.maxsize
+MIN_VALUE = -sys.maxsize - 1
+
 
 class NodoArbolBinario:
     def __init__(self, v, izq=None, der=None):
@@ -20,7 +23,7 @@ class ArbolBinario:
 
     def maximo(self, arbol):
         if arbol.raiz == None:
-            return -sys.maxsize - 1
+            return MIN_VALUE
         return max(arbol.valor, max(self.maximo(arbol.izq), self.maximo(arbol.der)))
     
     def minimo(self, arbol):
@@ -37,3 +40,11 @@ class ArbolBinario:
         if arbol.raiz == None:
             return 0
         return 1 + max(self.pisos(arbol.izq), self.pisos(arbol.der))
+
+    def es_arbol_binario(self, arbol, min=MIN_VALUE, max=MAX_VALUE):
+        if arbol.raiz == None:
+            return True
+        if arbol.valor < min or arbol.valor > max:
+            return False
+        return self.es_arbol_binario(arbol.raiz.izq, min, arbol.valor) and 
+        self.es_arbol_binario(arbol.raiz.der, arbol.raiz.val, MAX_VALUE)
