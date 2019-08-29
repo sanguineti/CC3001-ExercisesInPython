efb97e82e163f8d6cf5a1b6e983bdfa90e36dabe
F. Giovanni Sanguineti <franco.sanguineti@ug.uchile.cl>
2019-08-29 03:06:20 -0400

    added list generator

--- exercises/aux5/p2.py
+++ exercises/aux5/p2.py
@@ -1,7 +1,17 @@
 from P1 import ListaCircular
 
+
 def josephus(clista, k):
     nodo = clista.primero
     while clista.largo() > 1:
-        nodo = clista.eliminar(1+k) #El siguiente al eliminado
+        # El siguiente al eliminado (recordar que cuenta desde 1)
+        nodo = clista.eliminar(1+k)
     return nodo.val
+
+
+# Esta función genera una lista circular con cada elemento con el entero correspondiente a su posición en el primer conteo
+def generar_lista(cant):
+    lista = ListaCircular()
+    for i in range(1, cant + 1):
+        lista.agregar(i)
+    return lista
