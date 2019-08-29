72a710dce23d496cf734f8440fbf27931b62f9f7
F. Giovanni Sanguineti <franco.sanguineti@ug.uchile.cl>
2019-08-29 17:53:37 -0400

    added min, max, sum and pisos

--- exercises/aux5/p3.py
+++ exercises/aux5/p3.py
@@ -1,11 +1,13 @@
 import sys
 import math
 
+
 class NodoArbolBinario:
-    def __init__(self, v, izq = None, der = None):
+    def __init__(self, v, izq=None, der=None):
         self.val = v
         self.izq = izq
         self.der = der
+
 
 class ArbolBinario:
     def __init__(self):
@@ -18,5 +20,20 @@ class ArbolBinario:
 
     def maximo(self, arbol):
         if arbol.raiz == None:
-            return -sys.maxsize - 1 
-        return max(arbol.valor, max(max(arbol.izq), max(arbol.der)))
+            return -sys.maxsize - 1
+        return max(arbol.valor, max(self.maximo(arbol.izq), self.maximo(arbol.der)))
+    
+    def minimo(self, arbol):
+        if arbol.raiz == None:
+            return sys.maxsize
+        return min(arbol.valor, min(self.minimo(arbol.izq), self.minimo(arbol.der)))
+
+    def suma(self, arbol):
+        if arbol.raiz == None:
+            return 0
+        return arbol.val + self.suma(arbol.izq) + self.suma(arbol.der)
+
+    def pisos(self, arbol):
+        if arbol.raiz == None:
+            return 0
+        return 1 + max(self.pisos(arbol.izq), self.pisos(arbol.der))
