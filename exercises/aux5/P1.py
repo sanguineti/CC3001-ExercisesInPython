65f8557f9df69fdcba065c8f87fafbe2a14b9e5d
F. Giovanni Sanguineti <franco.sanguineti@ug.uchile.cl>
2019-08-29 02:36:23 -0400

    formated p1

--- exercises/aux5/P1.py
+++ exercises/aux5/P1.py
@@ -2,8 +2,10 @@ class NodoLista:
     def __init__(self, val, sig=None):
         self.val = val
         self.sig = sig
+
     def __repr__(self):
         return repr(self.val)
+
 
 class ListaCircular:
     def __init__(self):
@@ -12,24 +14,25 @@ class ListaCircular:
 
     def largo(self):
         return self.size
-    
+
     def obtener(self, i):
-        if self.size==0:
+        if self.size == 0:
             return -1
         buscador = self.primero
         for x in range(1, i):
             buscador = buscador.sig
         return buscador.val
-    
+
     def existe(self, num):
         buscador = self.primero
         for x in range(self.size):
             if int(buscador.val) == num:
                 return x
             buscador = buscador.sig
-        return -1      
+        return -1
+
     def eliminar(self, i):
-        if self.size==0:
+        if self.size == 0:
             return None
         buscador = self.primero
         if (i == 1):
@@ -46,13 +49,13 @@ class ListaCircular:
     def agregar(self, v):
         if self.size == 0:
             self.primero = NodoLista(v)
-            self.primero.sig = self.primero #Lista Circular
+            self.primero.sig = self.primero  # Lista Circular
             self.size += 1
             return
-    
+
         nodo_nuevo = NodoLista(v, self.primero)
         ultimo_antiguo = self.primero
-        for x in range(1,self.size):
+        for x in range(1, self.size):
             ultimo_antiguo = ultimo_antiguo.sig
         ultimo_antiguo.sig = nodo_nuevo
         self.size += 1
