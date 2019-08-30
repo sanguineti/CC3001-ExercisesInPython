9ce3483f00c9ffa181d8e8057edbada6df984509
F. Giovanni Sanguineti <franco.sanguineti@ug.uchile.cl>
2019-08-29 20:53:36 -0400

    finished refactor

--- exercises/aux5/p3.py
+++ exercises/aux5/p3.py
@@ -5,64 +5,69 @@ MAX_VALUE = sys.maxsize
 MIN_VALUE = -sys.maxsize - 1
 
 
-class NodoArbolBinario:
+class ArbolBinario:
     def __init__(self, v, izq=None, der=None):
         self.val = v
         self.izq = izq
         self.der = der
 
-
-class ArbolBinario:
-    def __init__(self, raiz=None):
-        self.raiz = raiz
-
     @staticmethod
     def numero_nodos(arbol):
-        if arbol.raiz == None:
+        if arbol == None:
             return 0
         return 1 + ArbolBinario.numero_nodos(arbol.der) + ArbolBinario.numero_nodos(arbol.izq)
 
     @staticmethod
     def maximo(arbol):
-        if arbol.raiz == None:
+        if arbol == None:
             return MIN_VALUE
         return max(arbol.valor, max(ArbolBinario.maximo(arbol.izq), ArbolBinario.maximo(arbol.der)))
 
     @staticmethod
     def minimo(arbol):
-        if arbol.raiz == None:
+        if arbol == None:
             return sys.maxsize
         return min(arbol.valor, min(ArbolBinario.minimo(arbol.izq), ArbolBinario.minimo(arbol.der)))
 
     @staticmethod
     def suma(arbol):
-        if arbol.raiz == None:
+        if arbol == None:
             return 0
         return arbol.val + ArbolBinario.suma(arbol.izq) + ArbolBinario.suma(arbol.der)
 
     @staticmethod
     def pisos(arbol):
-        if arbol.raiz == None:
+        if arbol == None:
             return 0
         return 1 + max(ArbolBinario.pisos(arbol.izq), ArbolBinario.pisos(arbol.der))
 
     @staticmethod
     def es_arbol_binario(arbol, min=MIN_VALUE, max=MAX_VALUE):
-        if arbol.raiz == None:
+        if arbol == None:
             return True
-        if arbol.raiz.val < min or arbol.raiz.val > max:
+        if arbol.val < min or arbol.val > max:
             return False
-        return ArbolBinario.es_arbol_binario(arbol.raiz.izq, min, arbol.raiz.val) and ArbolBinario.es_arbol_binario(arbol.raiz.der, arbol.raiz.val, MAX_VALUE)
+        return ArbolBinario.es_arbol_binario(arbol.izq, min, arbol.val) and ArbolBinario.es_arbol_binario(arbol.der, arbol.val, max)
 
 
-binario = NodoArbolBinario(5,
-                           NodoArbolBinario(3,
-                                            NodoArbolBinario(2),
-                                            NodoArbolBinario(4)),
-                           NodoArbolBinario(7,
-                                            NodoArbolBinario(6),
-                                            NodoArbolBinario(8,
-                                                             None,
-                                                             NodoArbolBinario(9))))
-arbol_binario = ArbolBinario(binario)
-print(ArbolBinario.es_arbol_binario(arbol_binario))
+binario = ArbolBinario(5,
+                       ArbolBinario(3,
+                                    ArbolBinario(2),
+                                    ArbolBinario(4)),
+                       ArbolBinario(7,
+                                    ArbolBinario(6),
+                                    ArbolBinario(8,
+                                                 None,
+                                                 ArbolBinario(9))))
+no_binario = ArbolBinario(5,
+                          ArbolBinario(3,
+                                           ArbolBinario(2),
+                                           ArbolBinario(16)),
+                          ArbolBinario(8,
+                                           ArbolBinario(7),
+                                           ArbolBinario(9)))
+
+# Casos de Prueba
+print(ArbolBinario.es_arbol_binario(binario))
+print(ArbolBinario.es_arbol_binario(no_binario))
+print(ArbolBinario.es_arbol_binario(None))
