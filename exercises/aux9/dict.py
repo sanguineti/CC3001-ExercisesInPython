548314a73768ea193469b7c6568fdecf55df4aa7
F. Giovanni Sanguineti <franco.sanguineti@ug.uchile.cl>
2019-10-18 02:11:25 -0300

    initial implementation of dict

---
+++ exercises/aux9/dict.py
@@ -0,0 +1,14 @@
+import numpy as np
+MIN_VALUE = -sys.maxsize - 1
+
+def ordernar_via_diccionario(arr):
+    diccionario = DiccionarioOrdenado()
+    for elemento in arr:
+        diccionario.insert(elemento)
+    arreglo = np.empty(len(arr), dtype=int)
+    minimo = MIN_VALUE
+    i = 0
+    while diccionario.sucesor(minimo) != None:
+        arreglo[i] = diccionario.sucesor(minimo)
+        minimo = diccionario.sucesor(minimo)
+    return arreglo     
